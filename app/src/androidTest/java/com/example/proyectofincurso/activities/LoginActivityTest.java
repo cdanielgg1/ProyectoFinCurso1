342bd33b5344be905d6f0ab96b3bdc06c14b8078
cdanielgg1 <cdanielgg@hotmail.com>
2019-02-23 12:04:22 +0100

    Ultima parte con login ok y subir foto ok, agregada intentos de test... falta de tiempo para poder seguir con el ejercicio

---
+++ app/src/androidTest/java/com/example/proyectofincurso/activities/LoginActivityTest.java
@@ -0,0 +1,22 @@
+package com.example.proyectofincurso.activities;
+
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void loginActivityTest() {
+    }
+}
